bd6ae61c10064e1bd65bf21682522cf8cdbc286e
Liladizilla <characharis01@gmail.com>
2026-03-10 06:19:06 +0000

    new files

---
+++ apps/desktop/src-tauri/src/lib.rs
@@ -0,0 +1,22 @@
+use tauri::Manager;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing::Level::INFO.into()),
+        )
+        .init();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            window.set_title("Lumina Studio").unwrap();
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
